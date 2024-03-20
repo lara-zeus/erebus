6b0c1fad3370403ea0c69e17057abaf3b5d73faa
Ash Monsh <wh7r.com@gmail.com>
2024-03-20 04:04:49 +0300

    remove Notifications

--- src/Configuration.php
+++ src/Configuration.php
@@ -8,27 +8,6 @@ trait Configuration
 {
     protected Closure | string $navigationGroupLabel = 'Erebus';
 
-    protected array $erebusModels = [
-        'Notification' => \LaraZeus\Erebus\Models\Notification::class,
-    ];
-
-    public function erebusModels(array $models): static
-    {
-        $this->erebusModels = $models;
-
-        return $this;
-    }
-
-    public function getErebusModels(): array
-    {
-        return $this->erebusModels;
-    }
-
-    public static function getModel(string $model): string
-    {
-        return (new static())::get()->getErebusModels()[$model];
-    }
-
     public function navigationGroupLabel(Closure | string $label): static
     {
         $this->navigationGroupLabel = $label;
