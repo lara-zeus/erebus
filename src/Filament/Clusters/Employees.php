6d20777fbb5466e738e47f413fa51e2fa3057fad
atmonshi <atmonshi@users.noreply.github.com>
2024-03-20 23:40:12 +0000

    Fix styling

--- src/Filament/Clusters/Employees.php
+++ src/Filament/Clusters/Employees.php
@@ -3,8 +3,6 @@
 namespace LaraZeus\Erebus\Filament\Clusters;
 
 use Filament\Clusters\Cluster;
-use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Contracts\View\View;
 
 class Employees extends Cluster
 {
